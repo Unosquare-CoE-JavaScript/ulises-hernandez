b607fdfb8e55d204ee993d5e81cfeb805ec8aea7
Ulises Hernandez <ulises.hernandez@unosquare.com>
2022-02-21 16:48:52 -0600

    Add: Express example project

---
+++ node-advanced-concepts/node-project/config/dev.js
@@ -0,0 +1,13 @@
+// Don't be like me and commit this file!
+// These keys have been disabled, but remain here for reference purposes.
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://ulises:PZHouO2BufjgONft@cluster0-shard-00-00.q6z6r.mongodb.net:27017,cluster0-shard-00-01.q6z6r.mongodb.net:27017,cluster0-shard-00-02.q6z6r.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-5zz6vu-shard-0&authSource=admin&retryWrites=true&w=majority',
+  //mongoURI: 'mongodb://ulises:PZHouO2BufjgONft@cluster0.q6z6r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIAIWSXFHRM7F6Z3NWQ',
+  secretAccessKey: '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'
+};
