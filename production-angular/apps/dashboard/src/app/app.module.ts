855a48a990ca4880a14410ceb97b218ebf48fc26
Ulises Hernandez <ulises.hernandez@unosquare.com>
2022-03-03 16:33:15 -0600

    Add: Key libs, mopdules and components files
    Add following files:
    
    - core-data lib
    - core-state lib
    - material lib
    - widgets components on dashboard app
    - home component on dahsboard app

--- production-angular/apps/dashboard/src/app/app.module.ts
+++ production-angular/apps/dashboard/src/app/app.module.ts
@@ -1,12 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { CoreDataModule } from '@production-angular/core-data';
+import {
+  CoreStateModule
+} from '@production-angular/core-state';
+import { MaterialModule } from '@production-angular/material';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RoutingModule } from './routing.module';
+import { WidgetDetailsComponent } from './widgets/widget-details/widget-details.component';
+import { WidgetsListComponent } from './widgets/widgets-list/widgets-list.component';
+import { WidgetsComponent } from './widgets/widgets.component';
+
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, WidgetsComponent, WidgetsListComponent, WidgetDetailsComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({}, {}),
+    CoreDataModule,
+    MaterialModule,
+    CoreStateModule,
+    RoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- production-angular/jest.config.js
+++ production-angular/jest.config.js
@@ -3,5 +3,9 @@ module.exports = {
     '<rootDir>/apps/dashboard',
     '<rootDir>/apps/api',
     '<rootDir>/libs/api-interfaces',
+    '<rootDir>/libs/core-data',
+    '<rootDir>/libs/corestate',
+    '<rootDir>/libs/material',
+    '<rootDir>/libs/core-state',
   ],
 };
