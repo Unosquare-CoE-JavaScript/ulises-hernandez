86188a7ef374e2fca0153138fb6100fd737c3cf2
Ulises Hernandez <ulises.hernandez@unosquare.com>
2022-03-09 14:44:20 -0600

    Add: e2e testing suite

--- production-angular/apps/dashboard-e2e/src/support/commands.ts
+++ production-angular/apps/dashboard-e2e/src/support/commands.ts
@@ -11,8 +11,17 @@
 declare namespace Cypress {
   interface Chainable<Subject> {
     login(email: string, password: string): void;
+    loadData(models: string[]): void;
+    checkLocation(path: string): void;
+    getEntities(model: string): void;
+    createEntity(model: string, mock: any): void;
+    updateEntity(model: string, mock: any): void;
+    deleteEntity(model: string, mock: any): void;
+    addEntity(model: string, mock: any): void;
   }
 }
+
+const API_URL = Cypress.env('apiUrl');
 //
 // -- This is a parent command --
 Cypress.Commands.add('login', (email, password) => {
@@ -29,3 +38,41 @@ Cypress.Commands.add('login', (email, password) => {
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('checkLocation', (route) => {
+  cy.location('pathname').should('equal', route);
+});
+
+Cypress.Commands.add('loadData', (models: string[]) => {
+  models.forEach(model => cy.getEntities(model));
+});
+
+Cypress.Commands.add('getEntities', (model) => {
+  cy.server();
+  cy.route('GET', `${API_URL}/${model}`, `fixture:${model}`);
+});
+
+Cypress.Commands.add('createEntity', (model, entity) => {
+  cy.server();
+  cy.route('POST', `${API_URL}/${model}`, { entity });
+  cy.addEntity(model, entity);
+});
+
+Cypress.Commands.add('updateEntity', (model, entity) => {
+  cy.server();
+  cy.route('PUT', `${API_URL}/${model}/${entity.id}`, { entity });
+  cy.addEntity(model, entity);
+});
+
+Cypress.Commands.add('deleteEntity', (model, entity) => {
+  cy.server();
+  cy.route('DELETE', `${API_URL}/${model}/${entity.id}`, { entity });
+  cy.getEntities(model);
+});
+
+Cypress.Commands.add('addEntity', (model, mock) => {
+  cy.server();
+  cy.fixture(model).then(collection => {
+    cy.route('GET', `${API_URL}/${model}`, [...collection, mock]).as(model);
+  });
+});
